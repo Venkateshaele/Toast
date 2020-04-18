d9b8a757a66746dbbeee9b94bcdd8ad05ae3ed41
Ajay Errabelli <aerrabelli@matrixstream.com>
2020-04-18 22:45:07 +0530

    Added Toast files

---
+++ Toast/Toast/Toast.h
@@ -0,0 +1,19 @@
+//
+//  Toast.h
+//  Toast
+//
+//  Created by matrixstream on 18/04/20.
+//  Copyright © 2020 matrixstream. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Toast.
+FOUNDATION_EXPORT double ToastVersionNumber;
+
+//! Project version string for Toast.
+FOUNDATION_EXPORT const unsigned char ToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Toast/PublicHeader.h>
+
+
